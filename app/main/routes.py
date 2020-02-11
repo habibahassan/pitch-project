70fd2aca2574bf034ee6b1c28e5cacb6e0be1068
habibahassan <halimaadan92@gmail.com>
2020-02-11 19:07:22 +0300

    added class blueprint on main/routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,15 +1,15 @@
-# from flask import render_template, request, Blueprint
-# from app.models import Pitch
+from flask import render_template, request, Blueprint
+from app.models import Pitch
 
-# main = Blueprint('main',__name__)
+main = Blueprint('main',__name__)
 
-# @main.route("/")
-# @main.route("/home")
-# def home():
-#     page = request.args.get('page', 1, type=int)
-#     pitches = Pitch.query.order_by(Pitch.date_posted.desc()).paginate(page=page, per_page=5)
-#     return render_template('home.html', pitches = pitches)
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    pitches = Pitch.query.order_by(Pitch.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', pitches = pitches)
 
-# @main.route("/about")
-# def about():
-#     return render_template('about.html', title = 'About')
+@main.route("/about")
+def about():
+    return render_template('about.html', title = 'About')

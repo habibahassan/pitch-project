436bbd72ac5b3ec239194b88502e3354a2c3dc32
habibahassan <halimaadan92@gmail.com>
2020-02-11 19:33:00 +0300

    added function for login on users/routes.py

--- app/users/routes.py
+++ app/users/routes.py
@@ -24,22 +24,22 @@ def register():
         return redirect(url_for('users.login'))
     return render_template('register.html',title = 'Register', form = form)
 
-# @users.route("/login", methods = ['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.home'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user and bcrypt.check_password_hash(user.password, form.password.data):
-#             login_user(user, remember = form.remember.data)
-#             next_page = request.args.get('next')
-#             return redirect(next_page) if next_page else redirect(url_for('main.home'))
+@users.route("/login", methods = ['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
 
-#             flash(f'Welcome back {user.username}')
-#         else:
-#             flash('Login Unsuccessful. Please check password and email', 'danger')
-#     return render_template('login.html',title = 'Login', form = form)
+            flash(f'Welcome back {user.username}')
+        else:
+            flash('Login Unsuccessful. Please check password and email', 'danger')
+    return render_template('login.html',title = 'Login', form = form)
 
 # @users.route("/logout")
 # def logout():

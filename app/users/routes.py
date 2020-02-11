beed6b9590961108820847cf43ab43e4c7735151
habibahassan <halimaadan92@gmail.com>
2020-02-11 19:32:22 +0300

    added function for register on users/routes.py

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,28 +1,28 @@
-# from flask import render_template, url_for, flash, redirect, request, Blueprint
-# from flask_login import login_user, current_user, logout_user, login_required
-# from app import db, bcrypt
-# from app.models import User, Pitch
-# from app.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
-#                                    RequestResetForm, ResetPasswordForm)
-# from app.users.utils import save_picture, send_reset_email
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from app import db, bcrypt
+from app.models import User, Pitch
+from app.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+                                   RequestResetForm, ResetPasswordForm)
+from app.users.utils import save_picture, send_reset_email
 
 
-# users = Blueprint('users',__name__)
+users = Blueprint('users',__name__)
 
-# @users.route("/register", methods= ['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.home'))
+@users.route("/register", methods= ['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
 
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-#         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash("Your account has been created! You are now ready to log in.", 'success')
-#         return redirect(url_for('users.login'))
-#     return render_template('register.html',title = 'Register', form = form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created! You are now ready to log in.", 'success')
+        return redirect(url_for('users.login'))
+    return render_template('register.html',title = 'Register', form = form)
 
 # @users.route("/login", methods = ['GET', 'POST'])
 # def login():

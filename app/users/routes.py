a096d830f00bd0f18266f6ddda666843bb31705a
habibahassan <halimaadan92@gmail.com>
2020-02-11 19:34:04 +0300

    added function for account on users/routes.py

--- app/users/routes.py
+++ app/users/routes.py
@@ -46,25 +46,25 @@ def logout():
     logout_user()
     return redirect(url_for('main.home'))
 
-# @users.route("/account", methods= ['GET', 'POST'])
-# @login_required
-# def account():
-#     form = UpdateAccountForm()
-#     if form.validate_on_submit():
-#         if form.picture.data:
-#             picture_file = save_picture(form.picture.data)
-#             current_user.image_file = picture_file
-#         current_user.username = form.username.data
-#         current_user.email = form.email.data
-#         db.session.commit()
-#         flash('Your account has been updated', 'success')
-#         return redirect(url_for('users.account'))
-#     elif request.method =='GET':
-#         form.username.data = current_user.username
-#         form.email.data = current_user.email
+@users.route("/account", methods= ['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('users.account'))
+    elif request.method =='GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
 
-#     image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
-#     return render_template('account.html',title = 'Account', image_file = image_file, form = form)
+    image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html',title = 'Account', image_file = image_file, form = form)
 
 # @users.route("/user/<string:username>")
 # def user_posts(username):
